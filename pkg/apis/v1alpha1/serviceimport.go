7538fca16fcad8cfdde484e693542eb4a751ccd6
Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@ledger.fr>
2024-12-03 14:22:59 +0100

    add convenient short name
    Signed-off-by: Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@ledger.fr>

--- pkg/apis/v1alpha1/serviceimport.go
+++ pkg/apis/v1alpha1/serviceimport.go
@@ -24,6 +24,7 @@ import (
 // +genclient
 // +kubebuilder:object:root=true
 // +kubebuilder:storageversion
+// +kubebuilder:resource:shortName=svcimport
 
 // ServiceImport describes a service imported from clusters in a ClusterSet.
 type ServiceImport struct {
