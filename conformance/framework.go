caeb28fbc1adc50d52c458220cbf79a7dea08236
Stephen Kitt <skitt@redhat.com>
2024-12-06 14:07:48 +0100

    Document exported funcs/types/etc., unless private
    Exported functions and types that aren't used outside their declaring
    package are unexported, the rest are documented, apart from type label
    constants, and Await functions which are removed (they are unused).
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- conformance/framework.go
+++ conformance/framework.go
@@ -19,88 +19,14 @@ package conformance
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"time"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	rest "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/remotecommand"
 )
-
-type (
-	DoOperationFunc func() (interface{}, error)
-	CheckResultFunc func(result interface{}) (bool, string, error)
-)
-
-// AwaitUntil periodically performs the given operation until the given CheckResultFunc returns true, an error, or a
-// timeout is reached.
-func AwaitUntil(opMsg string, doOperation DoOperationFunc, checkResult CheckResultFunc) interface{} {
-	result, errMsg, err := AwaitResultOrError(opMsg, doOperation, checkResult)
-	Expect(err).NotTo(HaveOccurred(), errMsg)
-
-	return result
-}
-
-func AwaitResultOrError(opMsg string, doOperation DoOperationFunc, checkResult CheckResultFunc) (interface{}, string, error) {
-	var finalResult interface{}
-	var lastMsg string
-
-	err := wait.PollUntilContextTimeout(context.Background(), 500*time.Millisecond,
-		10*time.Second, true, func(_ context.Context) (bool, error) {
-			result, err := doOperation()
-			if err != nil {
-				if IsTransientError(err, opMsg) {
-					return false, nil
-				}
-				return false, err
-			}
-
-			ok, msg, err := checkResult(result)
-			if err != nil {
-				return false, err
-			}
-
-			if ok {
-				finalResult = result
-				return true, nil
-			}
-
-			lastMsg = msg
-			return false, nil
-		})
-
-	errMsg := ""
-	if err != nil {
-		errMsg = "Failed to " + opMsg
-		if lastMsg != "" {
-			errMsg += ". " + lastMsg
-		}
-	}
-
-	return finalResult, errMsg, err
-}
-
-// identify API errors which could be considered transient/recoverable
-// due to server state.
-func IsTransientError(err error, opMsg string) bool {
-	if errors.IsInternalError(err) ||
-		errors.IsServerTimeout(err) ||
-		errors.IsTimeout(err) ||
-		errors.IsServiceUnavailable(err) ||
-		errors.IsUnexpectedServerError(err) ||
-		errors.IsTooManyRequests(err) {
-		fmt.Fprintf(GinkgoWriter, "Transient failure when attempting to %s: %v", opMsg, err)
-		return true
-	}
-
-	return false
-}
 
 func execCmd(k8s kubernetes.Interface, config *rest.Config, podName string, podNamespace string, command []string) ([]byte, []byte, error) {
 	req := k8s.CoreV1().RESTClient().Post().Resource("pods").Name(podName).Namespace(podNamespace).SubResource("exec")
