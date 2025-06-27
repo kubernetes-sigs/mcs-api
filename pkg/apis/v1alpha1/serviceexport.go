df7f8bd5465a15acf538d70b116900f8cf9ff289
Arthur Outhenin-Chalandre <arthur@cri.epita.fr>
2025-06-27 17:06:04 +0200

    apis: add ServiceExport condition type/reasons
    Signed-off-by: Arthur Outhenin-Chalandre <arthur@cri.epita.fr>

--- pkg/apis/v1alpha1/serviceexport.go
+++ pkg/apis/v1alpha1/serviceexport.go
@@ -66,6 +66,8 @@ const (
 	// service export has been recognized as valid by an mcs-controller.
 	// This will be false if the service is found to be unexportable
 	// (ExternalName, not found).
+	//
+	// Deprecated: use ServiceExportConditionAccepted instead
 	ServiceExportValid = "Valid"
 	// ServiceExportConflict means that there is a conflict between two
 	// exports for the same Service. When "True", the condition message
@@ -73,6 +75,8 @@ const (
 	// field(s) under contention, which cluster won, and why.
 	// Users should not expect detailed per-cluster information in the
 	// conflict message.
+	//
+	// Deprecated: use ServiceExportConditionConflicted instead
 	ServiceExportConflict = "Conflict"
 )
 
@@ -88,3 +92,134 @@ type ServiceExportList struct {
 	// +listType=set
 	Items []ServiceExport `json:"items"`
 }
+
+const (
+	// ServiceExportConditionAccepted is true when the Service Export is accepted.
+	// This does not indicate whether or not the configuration has been exported
+	// to a control plane / data plane.
+	//
+	//
+	// Possible reasons for this condition to be true are:
+	//
+	// * "Accepted"
+	//
+	// Possible reasons for this condition to be False are:
+	//
+	// * "NoService"
+	// * "InvalidServiceType"
+	//
+	// Controllers may raise this condition with other reasons,
+	// but should prefer to use the reasons listed above to improve
+	// interoperability.
+	ServiceExportConditionAccepted = "Accepted"
+
+	// ServiceExportReasonAccepted is used with the "Accepted" condition when the
+	// condition is True.
+	ServiceExportReasonAccepted = "Accepted"
+
+	// ServiceExportReasonNoService is used with the "Accepted" condition when
+	// the associated Service does not exist.
+	ServiceExportReasonNoService = "NoService"
+
+	// ServiceExportReasonInvalidServiceType is used with the "Accepted"
+	// condition when the associated Service has an invalid type
+	// (per the KEP at least the ExternalName type).
+	ServiceExportReasonInvalidServiceType = "InvalidServiceType"
+)
+
+const (
+	// ServiceExportConditionExported is true when the service is exported to some
+	// control plane or data plane.
+	//
+	//
+	// Possible reasons for this condition to be true are:
+	//
+	// * "Exported"
+	//
+	// Possible reasons for this condition to be False are:
+	//
+	// * "Pending"
+	// * "Failed"
+	//
+	// Possible reasons for this condition to be Unknown are:
+	//
+	// * "Pending"
+	//
+	// Controllers may raise this condition with other reasons,
+	// but should prefer to use the reasons listed above to improve
+	// interoperability.
+	ServiceExportConditionExported = "Exported"
+
+	// ServiceExportReasonExported is used with the "Exported" condition
+	// when the condition is True.
+	ServiceExportReasonExported = "Exported"
+
+	// ServiceExportReasonPending is used with the "Exported" condition
+	// when the service is going to be exported.
+	ServiceExportReasonPending = "Pending"
+
+	// ServiceExportReasonFailed is used with the "Exported" condition
+	// when the service failed to be exported.
+	ServiceExportReasonFailed = "Failed"
+)
+
+const (
+	// ServiceExportConditionConflicted indicates that the controller was unable
+	// to resolve conflict for a ServiceExport. This condition must be at
+	// least raised on the conflicting ServiceExport and is recommended to
+	// be raised on all on all the constituent `ServiceExport`s if feasible.
+	//
+	//
+	// Possible reasons for this condition to be true are:
+	//
+	// * "PortConflict"
+	// * "TypeConflict"
+	// * "SessionAffinityConflict"
+	// * "SessionAffinityConfigConflict"
+	// * "AnnotationsConflict"
+	// * "LabelsConflict"
+	//
+	// When multiple conflicts occurs the above reasons may be combined
+	// using commas.
+	//
+	// Possible reasons for this condition to be False are:
+	//
+	// * "NoConflicts"
+	//
+	// Controllers may raise this condition with other reasons,
+	// but should prefer to use the reasons listed above to improve
+	// interoperability.
+	ServiceExportConditionConflicted = "Conflicted"
+
+	// ServiceExportReasonPortConflict is used with the "Conflicted" condition
+	// when the exported service have a conflict related to port configuration.
+	// This includes when ports on resulting imported services would have
+	// duplicated names (including unnamed/empty name) or duplicated
+	// port/protocol pairs.
+	ServiceExportReasonPortConflict = "PortConflict"
+
+	// ServiceExportReasonTypeConflict is used with the "Conflicted" condition
+	// when the exported service have a conflict related to type.
+	ServiceExportReasonTypeConflict = "TypeConflict"
+
+	// ServiceExportReasonSessionAffinityConflict is used with the "Conflicted"
+	// condition when the exported service have a conflict related to session affinity.
+	ServiceExportReasonSessionAffinityConflict = "SessionAffinityConflict"
+
+	// ServiceExportReasonSessionAffinityConfigConflict is used with the
+	// "Conflicted" condition when the exported service have a conflict related
+	// to session affinity config.
+	ServiceExportReasonSessionAffinityConfigConflict = "SessionAffinityConfigConflict"
+
+	// ServiceExportReasonLabelsConflict is used with the "Conflicted"
+	// condition when the exported service have a conflict related to labels.
+	ServiceExportReasonLabelsConflict = "LabelsConflict"
+
+	// ServiceExportReasonAnnotationsConflict is used with the "Conflicted"
+	// condition when the exported service have a conflict related to annotations.
+	ServiceExportReasonAnnotationsConflict = "AnnotationsConflict"
+
+	// ServiceExportReasonNoConflicts is used with the "Conflicted" condition
+	// when the condition is False.
+	ServiceExportReasonNoConflicts = "NoConflicts"
+)
