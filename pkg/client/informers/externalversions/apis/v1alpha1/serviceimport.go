9b7f00fb13000e4a4f2838255c2b8423f31c8ace
Stephen Kitt <skitt@redhat.com>
2026-07-27 16:13:23 +0200

    Bump to k8s.io 0.34.10
    This is the latest point release of the oldest supported release
    branch.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- pkg/client/informers/externalversions/apis/v1alpha1/serviceimport.go
+++ pkg/client/informers/externalversions/apis/v1alpha1/serviceimport.go
@@ -62,13 +62,25 @@ func NewFilteredServiceImportInformer(client versioned.Interface, namespace stri
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.MulticlusterV1alpha1().ServiceImports(namespace).List(context.TODO(), options)
+				return client.MulticlusterV1alpha1().ServiceImports(namespace).List(context.Background(), options)
 			},
 			WatchFunc: func(options v1.ListOptions) (watch.Interface, error) {
 				if tweakListOptions != nil {
 					tweakListOptions(&options)
 				}
-				return client.MulticlusterV1alpha1().ServiceImports(namespace).Watch(context.TODO(), options)
+				return client.MulticlusterV1alpha1().ServiceImports(namespace).Watch(context.Background(), options)
+			},
+			ListWithContextFunc: func(ctx context.Context, options v1.ListOptions) (runtime.Object, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.MulticlusterV1alpha1().ServiceImports(namespace).List(ctx, options)
+			},
+			WatchFuncWithContext: func(ctx context.Context, options v1.ListOptions) (watch.Interface, error) {
+				if tweakListOptions != nil {
+					tweakListOptions(&options)
+				}
+				return client.MulticlusterV1alpha1().ServiceImports(namespace).Watch(ctx, options)
 			},
 		},
 		&pkgapisv1alpha1.ServiceImport{},
